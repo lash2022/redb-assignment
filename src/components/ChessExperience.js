6cf168d37009fd589ece39ad59209be8c66b9040
lasha giorgadze <lasha.giorgadze12@gmail.com>
2022-07-04 21:03:30 +0400

    main axis

---
+++ src/components/ChessExperience.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChessExperience = () => {
+  return (
+    <div>ChessExperience</div>
+  )
+}
+
+export default ChessExperience
---
+++ src/components/Grats.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Grats = () => {
+  return (
+    <div>Grats</div>
+  )
+}
+
+export default Grats
